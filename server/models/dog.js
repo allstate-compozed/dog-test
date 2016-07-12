6d5f855a9e7b94371bee6eb6159f00812a1d84d1
Chyld Medford <chyld.medford@gmail.com>
2016-07-12 14:50:46 -0500

    fin

---
+++ server/models/dog.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-use-before-define, func-names */
+
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+  name: { type: String,
+          required: true,
+          minlength: 3,
+          validate: { validator: duplicateDogNameValidator } },
+  age: { type: Number },
+  health: { type: Number, min: 0, max: 100 },
+  toy: { type: String, enum: ['Bones', 'Frisbee'] },
+  dateCreated: { type: Date, default: Date.now },
+});
+
+schema.methods.feed = function () {
+  this.health = this.health > 90 ? 100 : this.health + 10;
+};
+
+function duplicateDogNameValidator(name, cb) {
+  this.model('Dog').find({ name }, (err, dogs) => {
+    cb(!dogs.length);
+  });
+}
+
+module.exports = mongoose.model('Dog', schema);
---
+++ test/unit/dog.spec.js
@@ -0,0 +1,107 @@
+/* eslint-disable no-unused-expressions, no-underscore-dangle, func-names */
+
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const Dog = require('../../dst/models/dog');
+
+describe('Dog', () => {
+  beforeEach(() => {
+    sinon.stub(Dog, 'find').yields(null, []);
+  });
+
+  afterEach(() => {
+    Dog.find.restore();
+  });
+
+  describe('#feed', () => {
+    it('should add 10 to the dogs health', () => {
+      const d = new Dog({ name: 'fido',
+                          age: 3,
+                          health: 50,
+                          toy: 'Bones' });
+      d.feed();
+      expect(d.health).to.equal(60);
+    });
+
+    it('should add 5 to the dogs health - already at 95', () => {
+      const d = new Dog({ name: 'fido',
+                          age: 3,
+                          health: 95,
+                          toy: 'Bones' });
+      d.feed();
+      expect(d.health).to.equal(100);
+    });
+  });
+
+  describe('constructor', () => {
+    it('should create a dog object', (done) => {
+      const d = new Dog({ name: 'fido',
+                          age: 3,
+                          health: 50,
+                          toy: 'Bones' });
+      d.validate(err => {
+        expect(err).to.be.undefined;
+        expect(d.name).to.equal('fido');
+        expect(d._id).to.be.ok;
+        expect(d.dateCreated).to.be.ok;
+        done();
+      });
+    });
+
+    it('should NOT create a dog object - negative health', (done) => {
+      const d = new Dog({ name: 'fido',
+                          age: 3,
+                          health: -50,
+                          toy: 'Bones' });
+      d.validate(err => {
+        expect(err).to.be.ok;
+        done();
+      });
+    });
+
+    it('should NOT create a dog object - name missing', (done) => {
+      const d = new Dog({ age: 3,
+                          health: 50,
+                          toy: 'Bones' });
+      d.validate(err => {
+        expect(err).to.be.ok;
+        done();
+      });
+    });
+
+    it('should NOT create a dog object - name too short', (done) => {
+      const d = new Dog({ name: 'ab',
+                          age: 3,
+                          health: 50,
+                          toy: 'Bones' });
+      d.validate(err => {
+        expect(err).to.be.ok;
+        done();
+      });
+    });
+
+    it('should NOT create a dog object - invalid toy', (done) => {
+      const d = new Dog({ name: 'fido',
+                          age: 3,
+                          health: 50,
+                          toy: 'Cat' });
+      d.validate(err => {
+        expect(err).to.be.ok;
+        done();
+      });
+    });
+
+    it('should NOT create a dog object - duplicate dog found', done => {
+      Dog.find.yields(null, [{ name: 'max' }]);
+      const d = new Dog({ name: 'max',
+                          age: 3,
+                          health: 50,
+                          toy: 'Bones' });
+      d.validate(err => {
+        expect(err).to.be.ok;
+        sinon.assert.calledWith(Dog.find, { name: 'max' });
+        done();
+      });
+    });
+  });
+});
